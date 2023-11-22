d5bcd63ff86f34823b86ca0f2d4e0a9a254b662c
Андрей Кузнецов <blacksilver@ro.ru>
2023-11-23 00:06:29 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Telegram.Bot;
+
+namespace TelegramTestBot
+{
+    public class Program
+    {
+        private static TelegramBotClient botClient = new TelegramBotClient("6329059912:AAHOLuXfVWBpc9WHHdjzXRhmsXDP-2Varhw");
+
+        static void Main(string[] args)
+        {
+            var botInfo = botClient.GetMeAsync().Result;
+
+            Console.WriteLine($"Id: {botInfo.Id}");
+            Console.WriteLine($"Name: {botInfo.FirstName}");
+        }
+    }
+}
