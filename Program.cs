7e8b62c462914534b57f83c90ef286921642be05
Андрей Кузнецов <blacksilver@ro.ru>
2023-11-23 23:34:54 +0300

    Добавление обработчика для текстовых сообщений

--- Program.cs
+++ Program.cs
@@ -1,17 +1,36 @@
 ﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
 
 namespace TelegramTestBot
 {
     public class Program
     {
-        private static TelegramBotClient botClient = new TelegramBotClient("6329059912:AAHOLuXfVWBpc9WHHdjzXRhmsXDP-2Varhw");
+        private static readonly ITelegramBotClient _botClient = new TelegramBotClient("6329059912:AAHOLuXfVWBpc9WHHdjzXRhmsXDP-2Varhw");
 
         static void Main(string[] args)
         {
-            var botInfo = botClient.GetMeAsync().Result;
+            StartInfo();
 
-            Console.WriteLine($"Id: {botInfo.Id}");
-            Console.WriteLine($"Name: {botInfo.FirstName}");
+            var handler = new UpdateHandler();
+            var cancellationToken = new CancellationToken();
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { } //принимает все изменения
+            };
+
+            _botClient.StartReceiving(handler, receiverOptions, cancellationToken);
+            Console.ReadLine();
+        }
+
+        private static void StartInfo()
+        {
+            var botInfo = _botClient.GetMeAsync().Result;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Запуск бота: {botInfo.FirstName}");
+            Console.WriteLine($"Информация: {botInfo}");
+            Console.WriteLine();
+            Console.ResetColor();
         }
     }
 }
---
+++ UpdateHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace TelegramTestBot
+{
+    public class UpdateHandler : IUpdateHandler
+    {
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Type == UpdateType.Message)
+            {
+                var message = update.Message;
+                var userName = message.Chat.Username;
+
+                if (message.Text.ToLower() == "/start")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, $"Сообщение отправлено. Username пользователя: {userName}");
+                    Console.WriteLine($"Сообщение отправлено. Username пользователя: {userName}");
+                }
+            }
+        }
+
+        public async Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Произошла ошибка: {JsonSerializer.Serialize(exception)}");
+        }
+    }
+}
